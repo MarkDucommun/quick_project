4eb7fbfd2c71b97e5c123d27877acbd418683ef1
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 18:22:34 -0500

    adds rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickProject::Application.config.secret_token = '9f376c0a229e70369dd967fc2803edee879c62fe8845b689efc71df9885dbdcc01bb6e621a6d46e1cc651c4efe6b7691082749234073231668d26f2ce47d6061'
