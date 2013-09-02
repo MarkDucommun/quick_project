0642135277ca9bd8f98b2be26f6bbfc86c7299f8
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-02 08:00:40 -0500

    working on frontpage

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,3 +1,5 @@
 class Puzzle < ActiveRecord::Base
   validates_presence_of :name, :x_dimension, :y_dimension
+
+  attr_accessible :name, :x_dimension, :y_dimension
 end
--- spec/features/puzzle_spec.rb
+++ spec/features/puzzle_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe "Index page" do
   context "Anonymous visitor" do
     it "can click a link to play a puzzle" do
+      Puzzle.create(name: "First", x_dimension: 5, y_dimension: 5)
+
       visit root_url
 
       click_link "First"
